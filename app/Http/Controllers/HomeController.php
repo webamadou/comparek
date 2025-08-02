62cc9916b407b71dee8e75fc4b9595685d582b45
webamadou <paab26@live.fr>
2025-08-02 18:09:52 +0000

    Correct request if no article

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     {
         $illus = collect([2, 3, 5])->random();
         $latestPost = Post::orderBy('published_at', 'desc')->with('category')->first();
-        $articles = Post::where('id', '!=', $latestPost->id)->with('category')->take(4)->get();
+        $articles = Post::where('id', '!=', $latestPost?->id)->with('category')->take(4)->get();
         return view('home',  compact('illus', 'articles', 'latestPost'));
     }
 }
