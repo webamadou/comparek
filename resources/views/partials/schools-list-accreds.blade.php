4983b408525424bae17d15cb798e42d24aa5f64a
webamadou <paab26@live.fr>
2025-08-06 16:25:44 +0000

    Correct no link is active after filter

--- resources/views/partials/schools-list-accreds.blade.php
+++ resources/views/partials/schools-list-accreds.blade.php
@@ -13,17 +13,19 @@
     <div class="row mb-0 school-row p-0 school-row-wrapper">
         <div class="school-logo col-sm-2 col-md-2">
             @if($school->images && !empty($school->images->path) && Storage::disk('public')->exists($school->images->path))
-                <a href="{{ route('view_school', $school) }}">
+                <a href="{{ route('view_school', $school) }}" title="{{ $school->name }}">
                     <img src="{{ Storage::disk('public')->url($school->images->path) }}" width="100%" alt="{{ $school->images->path }}">
                 </a>
             @else
-                <a href="{{ route('view_school', $school->slug) }}">
+                <a href="{{ route('view_school', $school->slug) }}" title="{{ $school->name }}">
                     <img src="{{ asset('frontv1/img/illustration/default-img.png') }}" width="100%" alt="Default school image">
                 </a>
             @endif
         </div>
         <div class="school-text col-sm-10 col-md-10">
-            <h3 class="card-title">{{ $school->name }}</h3>
+            <h3 class="card-title">
+                <a href="{{ route('view_school', $school) }}" title="{{ $school->name }}">{{ $school->name }}</a>
+            </h3>
             {!! $school->description !!}
             <ul class="accreditation-list">
                 @foreach($accreditations as $accred)
