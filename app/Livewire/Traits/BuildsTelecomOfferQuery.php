f21c1d8efa9f2eac30aeddf026c9c29fab5c1796
webamadou <paab26@live.fr>
2025-09-26 07:09:28 +0000

    Rewrite request for telecom's mobile offers in telecom comparison

--- app/Livewire/Traits/BuildsTelecomOfferQuery.php
+++ app/Livewire/Traits/BuildsTelecomOfferQuery.php
@@ -132,40 +132,43 @@ trait BuildsTelecomOfferQuery
                 'offer.operator:id,name',
                 'offer.operator.images' => fn ($query) => $query->select('id', 'imageable_id', 'imageable_type', 'path')
             ])
-
             ->when($this->validityLength > 0 && $this->validityLength < 30, fn ($query) => $query->where('validity_length', $this->validityLength))
             ->when($this->validityLength >= 30, fn ($query) => $query->where('validity_length', '>=', $this->validityLength))
-            ->when($this->data > 0 && $this->data < 1024, function ($query) {
-                return $query->whereRaw("
-                    CASE
-                        WHEN data_volume_unit = 'Go' THEN data_volume_value * 1024
-                        WHEN data_volume_unit = 'Mo' THEN data_volume_value
-                        ELSE NULL
-                    END <= ?
-                ", [$this->data]);
-            })
-            ->when($this->data >= 1024, function ($query) {
-                return $query->whereRaw("
-                    CASE
-                        WHEN data_volume_unit = 'Go' THEN data_volume_value * 1024
-                        WHEN data_volume_unit = 'Mo' THEN data_volume_value
-                        ELSE NULL
-                    END >= ?
-                ", [$this->data]);
+            ->when($this->data > 0, function ($query) {
+                if ($this->data < 1024*5) {
+                    $min = max($this->data - 20, 0);
+                    $max = $this->data + 20;
+
+                    return $query->whereRaw("
+                        CASE
+                            WHEN data_volume_unit = 'Go' THEN data_volume_value * 1024
+                            WHEN data_volume_unit = 'Mo' THEN data_volume_value
+                            ELSE NULL
+                        END BETWEEN ? AND ?
+                    ", [$min, $max]);
+                } else {
+                    return $query->whereRaw("
+                        CASE
+                            WHEN data_volume_unit = 'Go' THEN data_volume_value * 1024
+                            WHEN data_volume_unit = 'Mo' THEN data_volume_value
+                            ELSE NULL
+                        END >= ?
+                    ", [5000]);
+                }
             })
             ->when($this->voiceMinutes > 0, function ($query) {
                 return $this->voiceMinutes < 1000
-                    ? $query->where('voice_minutes', '<=', $this->voiceMinutes)
+                    ? $query->where('voice_minutes', '>=', $this->voiceMinutes - 10)->where('voice_minutes', '<=', $this->voiceMinutes + 10)
                     : $query->where('voice_minutes', '>=', $this->voiceMinutes);
             })
             ->when($this->sms_nbr > 0, function ($query) {
                 return $this->sms_nbr < 1000
-                    ? $query->where('sms_nbr', '<=', $this->sms_nbr)
+                    ? $query->where('sms_nbr', '>=', $this->sms_nbr - 10)->where('sms_nbr', '<=', $this->sms_nbr + 10)
                     : $query->where('sms_nbr', '>=', $this->sms_nbr);
             })
             ->when($this->phoneCredit > 0, function ($query) {
-                return $this->phoneCredit < 5000
-                    ? $query->where('phone_credit', '<=', $this->phoneCredit)
+                return $this->phoneCredit < 8000
+                    ? $query->where('phone_credit', '>=', $this->phoneCredit)->where('phone_credit', '<=', $this->phoneCredit + 50)
                     : $query->where('phone_credit', '>=', $this->phoneCredit);
             })
             ->when($this->price > 0, function ($query) {
